3a593d0ee8eab116a9de337669bf335e36ff8e97
fabiobitencourt93 <fabiobitencourt.93@gmail.com>
2026-06-03 20:55:34 -0300

    NOVO

---
+++ tela_docente/docente.js
@@ -0,0 +1,30 @@
+function atualizarRelogio() {
+    const agora = new Date(); // Obtém a data e hora atual
+    
+    // Formata a hora, minutos e segundos para ter sempre 2 dígitos
+    const horas = String(agora.getHours()).padStart(2, '0');
+    const minutos = String(agora.getMinutes()).padStart(2, '0');
+    const segundos = String(agora.getSeconds()).padStart(2, '0');
+    
+    // Formata a data (dia/mês/ano)
+    
+    const dia = String(agora.getDate()).padStart(2, '0');
+    const mes = String(agora.getMonth() + 1).padStart(2, '0');
+    const ano = agora.getFullYear();
+    
+    // Exibe no HTML
+    document.getElementById('relogio').textContent = `${horas}:${minutos}:${segundos}`;
+    document.getElementById('data').textContent = `${dia}/${mes}/${ano}`;
+}
+
+// Executa a função imediatamente ao carregar e depois a cada 1 segundo
+atualizarRelogio();
+setInterval(atualizarRelogio, 1000);
+
+document.getElementById("logout").addEventListener("click", function () {
+    window.location.href = "../tela_login/index.html";
+});
+
+document.getElementById("logout").addEventListener("click", function () {
+    window.location.href = "../tela_login/index.html";
+});
---
+++ tela_login/index.js
@@ -0,0 +1,76 @@
+const id = document.getElementById('registro_id')
+const senha = document.getElementById('senha_id')
+const botao = document.getElementById('entrar_btn')
+
+let tela_destino = "../tela_login/index.html"
+
+const docente = document.getElementById('docente_btn')
+    docente.addEventListener("click", ()=>{
+        tela_destino = "../tela_docente/docente.html"
+
+    })
+
+    const coord = document.getElementById('coord_btn')
+    coord.addEventListener("click", ()=>{
+        tela_destino = "../tela_coordenador/coordenador.html"
+    })
+
+    const secretaria = document.getElementById('secretaria_btn')
+    secretaria.addEventListener("click", ()=>{
+        tela_destino = "../tela_secretaria/secretaria.html"
+    })
+
+
+function validaUsuario(){
+    if(id.value === "" || senha.value === ""){
+        alert("Credenciais Inválidas, tente novamente.")
+    }
+    else{
+            if(tela_destino === "../tela_login/index.html"){
+            alert("Selecione um perfil")
+            return
+        }
+        else{
+            window.location.href = tela_destino
+        }
+        
+    }
+    id.value = ""
+    senha.value = ""
+}
+
+botao.addEventListener("click", validaUsuario)
+
+
+const secretariaBtn = document.getElementById("secretaria_btn");
+const coordBtn = document.getElementById("coord_btn");
+const docenteBtn = document.getElementById("docente_btn");
+
+let perfilSelecionado = "";
+
+function selecionarPerfil(botao, perfil) {
+
+    // Remove seleção anterior
+    secretariaBtn.classList.remove("perfil-ativo");
+    coordBtn.classList.remove("perfil-ativo");
+    docenteBtn.classList.remove("perfil-ativo");
+
+    // Adiciona ao botão clicado
+    botao.classList.add("perfil-ativo");
+
+    perfilSelecionado = perfil;
+
+    console.log("Perfil selecionado:", perfilSelecionado);
+}
+
+secretariaBtn.addEventListener("click", () => {
+    selecionarPerfil(secretariaBtn, "secretaria");
+});
+
+coordBtn.addEventListener("click", () => {
+    selecionarPerfil(coordBtn, "coordenador");
+});
+
+docenteBtn.addEventListener("click", () => {
+    selecionarPerfil(docenteBtn, "docente");
+});
